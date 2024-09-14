d80ae8a68f144b2b9a0033a9c64984417a578ab1
Bryan Gomes <bryanpgomes@gmail.com>
2024-09-13 20:55:35 -0400

    Fix handleHeroSelection

--- app/page.tsx
+++ app/page.tsx
@@ -61,83 +61,90 @@ export default function Home() {
     {
       id: 1,
       name: "Abrams",
-      counters: [1, 3,4],
-      imagePath:'/assets/images/heroes/Abrams_card.png',
+      counters: [1, 3, 4],
+      imagePath: '/assets/images/heroes/Abrams_card.png',
     },
     {
       id: 2,
       name: "Bebop",
       counters: [4],
-      imagePath:'/assets/images/heroes/Bebop_card.png',
+      imagePath: '/assets/images/heroes/Bebop_card.png',
     },
     {
       id: 3,
       name: "Dynamo",
       counters: [4],
-      imagePath:'/assets/images/heroes/Dynamo_card.png',
+      imagePath: '/assets/images/heroes/Dynamo_card.png',
     },
     {
       id: 4,
       name: "Grey Talon",
       counters: [4],
-      imagePath:'/assets/images/heroes/Grey_Talon_card.png',
+      imagePath: '/assets/images/heroes/Grey_Talon_card.png',
     },
     {
       id: 5,
       name: "Haze",
       counters: [2],
-      imagePath:'/assets/images/heroes/Haze_card.png',
+      imagePath: '/assets/images/heroes/Haze_card.png',
     },
     {
       id: 6,
       name: "Infernus",
-      counters: [1,3],
-      imagePath:'/assets/images/heroes/Infernus_card.png',
+      counters: [1, 3],
+      imagePath: '/assets/images/heroes/Infernus_card.png',
     },
-    
+
   ];
 
   const handleHeroSelection = (singleHero: Hero) => {
     // Everytime a hero is clicked it should grab the icons as well
-    setSelectedHeroes((prev: Hero[])=> ([...prev, singleHero]))
+    setSelectedHeroes((prev: Hero[]) => {
+      const isHeroSelect = prev.find((element)=> element.id = singleHero.id)
+      if(isHeroSelect){
+        return prev.filter((hero)=> hero.id !== singleHero.id);
+      }
+      return [...prev, singleHero];
+    }
+    )
   };
 
   return (
     <div className="">
       <main className="flex flex-col items-center">
-          <h1 className="font-serif">Select your opponents:</h1>
+        <h1 className="font-serif">Select your opponents:</h1>
         <section className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-        {arrayOfHeroes.map((singleHero: Hero)=> 
-        <Image
-          className="transition duration-500 hover:scale-110 hover:bg-stone-400 hover:cursor-pointer"
-          key={singleHero.id}
-          src={singleHero.imagePath}
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-          onClick={()=> setSelectedHeroes((prev: Hero[])=> ([...prev, singleHero]))}
-        />
-        )}
+          {arrayOfHeroes.map((singleHero: Hero) =>
+            <Image
+              className="transition duration-500 hover:scale-110 hover:bg-stone-400 hover:cursor-pointer"
+              key={singleHero.id}
+              src={singleHero.imagePath}
+              alt="Next.js logo"
+              width={180}
+              height={38}
+              priority
+              onClick={() => setSelectedHeroes((prev: Hero[]) => ([...prev, singleHero]))}
+            />
+          )}
         </section>
         <section className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
-        {selectedHeroes.map((singleHero: Hero, index)=> 
-        <Image
-          className="transition duration-500 hover:scale-110 hover:bg-stone-400 hover:cursor-pointer"
-          key={index}
-          src={singleHero.imagePath}
-          alt="Next.js logo"
-          width={100}
-          height={38}
-          priority
-          onClick={()=> setSelectedHeroes((prev: Hero[])=> ([...prev, singleHero]))}
-        />
-        )}
+          {selectedHeroes.map((singleHero: Hero, index) =>
+            <Image
+              className="transition duration-500 hover:scale-110 hover:bg-stone-400 hover:cursor-pointer"
+              key={index}
+              src={singleHero.imagePath}
+              alt="Next.js logo"
+              width={100}
+              height={38}
+              priority
+              onClick={() => setSelectedHeroes((prev: Hero[]) => ([...prev, singleHero]))}
+            />
+          )}
         </section>
-        
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-       
+
       </footer>
     </div>
   );
